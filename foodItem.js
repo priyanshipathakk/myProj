9da343cc674293fd52f805f610ba3d2afba62027
Priyanshi <priyanshi2007.pathak@gmail.com>
2025-06-16 21:17:41 +0530

    myProj

---
+++ foodItem.js
@@ -0,0 +1,353 @@
+const foodItem = [
+    {
+        id: 1,
+        name: 'Veggie Pizza',
+        category : 'PizzaCorner',
+        rating: 4.3,
+        price : 129,
+        img :  'images/veggie pizza.jpeg',
+        quantity: 1
+    },
+    {
+        id: 2,
+        name: 'Margherita pizza',
+        category: 'PizzaCorner',
+        rating: 4.3,
+        price: 139,
+        img: 'images/margherita pizza.jpeg',
+        quantity: 1
+    },
+   {
+    id: 3,
+    name: 'BBQ chicken pizza',
+    category: 'PizzaCorner',
+    rating: 4.3,
+    price: 150,
+    img: 'images/BBQ  pizza.jpeg',
+    quantity: 1
+   },
+   {
+     id: 4,
+     name: 'Cheesy pizza',
+     category: 'PizzaCorner',
+     rating: 4.3,
+     price: 139,
+     img: 'images/Cheesy Pizza.jpeg',
+     quantity: 1
+   },
+   {
+    id: 6,
+    name: 'Pepperoni pizza',
+    category: 'PizzaCorner',
+    rating: 4.3,
+    price: 169,
+    img: 'images/pepperoni pizza.jpeg',
+    quantity: 1
+   },
+   {
+    id: 6,
+    name: 'Paneer tikka pizza',
+    category: 'PizzaCorner',
+    rating: 4.2,
+    price: 159,
+    img: 'images/Paneer tikka pizza.jpg',
+    quantity: 1
+   },
+   {
+     id : 7,
+     name : 'Afghani biryani',
+     category : 'Biryanikhazana',
+     rating : 4.1,
+     price :  209,
+     img : 'images/afghani biryani.jpg',
+     quantity : 1
+ },
+ {
+    id : 8,
+    name :'Ambur biryani',
+    category  : 'Biryanikhazana',
+    rating : 4.3,
+    price : 234,
+    img : 'images/Ambur-Chicken-Biryani.jpg',
+    quantity : 1
+ },
+ {
+    id : 9,
+    name : 'Bombay biryani',
+    category : 'Biryanikhazana',
+    rating : 4.1,
+    price : 254,
+    img : 'images/bombay biryani.jpeg',
+    quantity : 1
+ },
+  { 
+    id  : 10,
+    name : 'Hyderabadi biryani',
+    category :'Biryanikhazana',
+    rating : 4.5,
+    price : 256,
+    quantity : 1
+  },
+  { 
+    id : 11,
+    name :'Sindhi biryani',
+    category : 'Biryanikhazana',
+    rating : 4.6,
+    price : 249,
+    img : 'images/sindhi-biryani.jpg',
+    quantity : 1
+  },
+   {
+    id : 12,
+    name : 'Veg biryani',
+    category : 'Biryanikhazana',
+    rating : 4.3,
+    price : 249,
+    img : 'images/veg biryani.jpg',
+    quantity : 1
+   },
+    {
+      id : 13,
+    name : 'Malabar biryani',
+    category : 'Biryanikhazana',
+    rating : 4.3,
+    price : 209,
+    img : 'images/malabar-chicken.avif',
+    quantity : 1
+    },
+   {
+    id : 14,
+    name : 'Chilli paneer',
+    category : 'Chinesemania',
+    rating : 4.2,
+    price :99,
+    img : 'images/chilli paneer.jpeg',
+    quantity : 1
+   },
+   {
+    id : 15,
+    name : 'Chilli fried rice',
+    category :'Chinesemania',
+    rating : 4.6,
+    price : 109,
+    quantity : 1
+   },
+   {
+    id : 16,
+    name : 'Chowmien',
+    category : 'Chinesemania',
+    rating : 4.3,
+    price : 110,
+    img : 'images/chowmien1.jpg',
+    quantity : 1
+   },
+   {
+    id : 17,
+    name : 'Manchow soup',
+    category : 'Chinesemania',
+    rating : 4.2,
+    price : 99,
+    img :'images/manchow-soup.jpg',
+    quantity : 1,
+   },
+   {
+    id : 18,
+    name : 'Manchurian',
+    category : 'Chinesemania',
+    rating :4.2,
+    price : 120,
+    img : 'images/manchurian.jpeg',
+    quantity : 1
+   },
+   {
+    id : 19,
+    name : 'Spring roll',
+    category : 'Chinesemania',
+    rating : 4.1,
+    price : 110,
+    img : 'images/spring-roll.jpg',
+    quantity : 1
+   },
+   {
+    id : 20,
+    name : 'French fries',
+    category : 'Chinesemania',
+    rating : 4.1,
+    price : 80,
+    img : 'images/French fries.jpg',
+    quantity : 1
+   },
+   {
+    id : 21,
+    name : 'Chilli potato',
+    category : 'Chinesemania',
+    rating : 4.1,
+    price : 110,
+    img : 'images/chilli potato.jpg',
+    quantity : 1
+   },
+   {
+     id : 22,
+    name : 'Honey chilli potato',
+    category : 'Chinesemania',
+    rating : 4.1,
+    price : 120,
+    img :'images/honey potato.jpg',
+    quantity : 1
+   },
+   {
+     id : 23,
+    name : 'Dosa',
+    category : 'South-indian',
+    rating : 4.4,
+    price : 100,
+    img :'images/dosa.png',
+    quantity : 1
+   },
+    {
+     id : 24,
+    name : 'Idli-sambhar',
+    category : 'South-indian',
+    rating : 4.4,
+    price : 69,
+    img :'images/idli.jpg',
+    quantity : 1
+   },
+    {
+     id : 24,
+    name : 'Medu vada',
+    category : 'South-indian',
+    rating : 4.4,
+    price : 50,
+    img :'images/medu vada.jpeg',
+    quantity : 1
+   },
+    {
+     id : 25,
+    name : 'Uttapam',
+    category : 'South-indian',
+    rating : 4.4,
+    price : 70,
+    img :'images/uttapam.jpg',
+    quantity : 1
+   },
+    {
+     id : 26,
+    name : 'Payasam',
+    category : 'South-indian',
+    rating : 4.4,
+    price : 80,
+    img :'images/payasam kheer.jpeg',
+    quantity : 1
+   },
+   {
+     id : 27,
+    name : 'Paneer butter masala',
+    category : 'Paneermania',
+    rating : 4.2,
+    price : 120,
+    img :'images/Paneer-Butter-Masala.jpg',
+    quantity : 1
+   },
+    {
+     id : 28,
+    name : 'Palak paneer',
+    category : 'Paneermania',
+    rating : 4.2,
+    price : 110,
+    img :'images/palak paneer.jpeg',
+    quantity : 1
+   },
+   {
+     id : 29,
+    name : 'Kadai paneer',
+    category : 'Paneermania',
+    rating : 4.2,
+    price : 130,
+    img :'images/kadai paneer.jpg',
+    quantity : 1
+   },
+   {
+     id : 30,
+    name :  'Matar paneer',
+    category : 'Paneermania',
+    rating : 4.2,
+    price : 130,
+    img :'images/matar-paneer.jpg',
+    quantity : 1
+   },
+   {
+     id : 31,
+    name : 'Achari paneer',
+    category : 'Paneermania',
+    rating : 4.2,
+    price : 140,
+    img :'images/achari paneer.jpeg',
+    quantity : 1
+   },
+   {
+     id : 32,
+    name : 'Chilli paneer',
+    category : 'Paneermania',
+    rating : 4.2,
+    price : 130,
+    img :'images/chilli paneer.jpeg',
+    quantity : 1
+   },
+  {
+    id : 33,
+    name : 'Tiramisu',
+    category : 'Desserts',
+    rating : 4.7,
+    price : 200,
+    img : 'images/tiramisu.jpg',
+    quantity : 1
+  },
+  {
+    id :34  ,
+    name : 'Chocolate mousse',
+    category : 'Desserts',
+    rating : 4.7,
+    price : 190,
+    img : 'images/mousse.webp',
+    quantity : 1
+  },
+  {
+    id : 35,
+    name : 'Gulab Jamun (1pc)',
+    category : 'Desserts',
+    rating : 4.7,
+    price : 50,
+    img : 'images/gulab jamun.webp',
+    quantity : 1
+  },
+  {
+    id : 36,
+    name : 'Rasmalai (2pc)',
+    category : 'Desserts',
+    rating : 4.7,
+    price : 60,
+    img :'images/Rasmalai.jpg',
+    quantity : 1
+  },
+  {
+    id : 37 ,
+    name : 'Blueberry cheesecake',
+    category : 'Desserts',
+    rating : 4.7,
+    price : 150,
+    img : 'images/blueberry cheesecke.webp',
+    quantity : 1
+  },
+  {
+    id : 38 ,
+    name : 'Brownie',
+    category : 'Desserts',
+    rating : 4.7,
+    price : 99,
+    img : 'images/brownie.jpeg',
+    quantity : 1
+  },
+]
+
+ {foodItem};
